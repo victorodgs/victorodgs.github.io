6655473caf320a72967d14c467adad5f685600c2
Victor Costa <victorodgs@gmail.com>
2020-03-03 00:19:20 -0300

    Clean Repo

--- src/helpers/If.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-else-return */
-// Helper to do conditional rendering
-export default props => {
-    if (props.test) {
-        return props.children;
-    } else {
-        return false;
-    }
-};
