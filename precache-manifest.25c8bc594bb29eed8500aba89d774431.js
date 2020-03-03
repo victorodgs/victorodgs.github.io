74de5b61f060807cd8630e12759651093b111646
Victor Costa <victorodgs@gmail.com>
2020-03-03 00:12:08 -0300

    Updates

---
+++ precache-manifest.25c8bc594bb29eed8500aba89d774431.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d5ff4d6c32671933621a043d8d1adf",
+    "url": "/jokenorris/index.html"
+  },
+  {
+    "revision": "c3cb43f7cdb77f02fec9",
+    "url": "/jokenorris/static/css/main.014da149.chunk.css"
+  },
+  {
+    "revision": "0b01bb83798c1f899a7d",
+    "url": "/jokenorris/static/js/2.d6dd4184.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jokenorris/static/js/2.d6dd4184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3cb43f7cdb77f02fec9",
+    "url": "/jokenorris/static/js/main.207c3ad0.chunk.js"
+  },
+  {
+    "revision": "2c3511e158fe63ce943b",
+    "url": "/jokenorris/static/js/runtime-main.8c6514de.js"
+  },
+  {
+    "revision": "b138b0e86cdc3672ada296a8fcb1da48",
+    "url": "/jokenorris/static/media/Kelson_Lt.b138b0e8.otf"
+  },
+  {
+    "revision": "695f7163255ed6c0a93c1fcda7189d1c",
+    "url": "/jokenorris/static/media/Kelson_Md.695f7163.otf"
+  },
+  {
+    "revision": "192ae25c5ba25208d0e236441fe6fad5",
+    "url": "/jokenorris/static/media/jokenorris-site-logo.192ae25c.png"
+  },
+  {
+    "revision": "3b874fd20821f2a0e3ce48bfcf734fc7",
+    "url": "/jokenorris/static/media/jokenorris.3b874fd2.jpg"
+  },
+  {
+    "revision": "ea8a8e5b54fb90e119f1960a90252054",
+    "url": "/jokenorris/static/media/risada-do-bola.ea8a8e5b.mp3"
+  }
+]);
